507a3d75e1fb4325fbdd1f7b187235cd97c4f1c9
MindaugasK <mindaugas.krup@gmail.com>
2021-09-30 23:28:49 +0300

    add proxy

--- ASP.NETCoreWebApplication/Models/Photo.cs
+++ ASP.NETCoreWebApplication/Models/Photo.cs
@@ -1,7 +1,19 @@
-﻿namespace ASP.NETCoreWebApplication.Models
+﻿using System.Net;
+using System.Runtime.InteropServices;
+
+namespace ASP.NETCoreWebApplication.Models
 {
     public class Photo
     {
-        
+        private IPAddress href;
+        private int width;
+        private int height;
+
+        public Photo(IPAddress href, int width, int height)
+        {
+            this.href = href;
+            this.width = width;
+            this.height = height;
+        }
     }
 }
--- ASP.NETCoreWebApplication/Models/Proxy.cs
+++ ASP.NETCoreWebApplication/Models/Proxy.cs
@@ -1,7 +1,49 @@
-﻿namespace ASP.NETCoreWebApplication.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ASP.NETCoreWebApplication.Models
 {
     public class Proxy
     {
-        
+        private IPAddress ip;
+        private String username;
+        private String password;
+        private int port;
+        Proxy(IPAddress ip, String username, String password, int port)
+        {
+            this.ip = ip;
+            this.username = username;
+            this.password = password;
+            this.port = port;
+        }
+
+        //redirect traffic through proxy
+        public HttpWebResponse Route(HttpWebRequest request, System.Net.WebHeaderCollection headers, string body = "", string method = "GET")
+        {
+            WebProxy myproxy;
+            myproxy = new WebProxy(this.ip.ToString(), this.port);
+            if (this.username == "")
+            {
+                myproxy.Credentials = new NetworkCredential(this.username, this.password);
+            }
+            myproxy.BypassProxyOnLocal = false;
+            request.Proxy = myproxy;
+            request.Method = method;
+            request.Headers = headers;
+            if (method == "POST")
+            {
+                var data = Encoding.ASCII.GetBytes(body);
+                request.ContentLength = data.Length;
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+            return response;
+        }
+
     }
 }

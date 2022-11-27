7038e82e1db5243d22ae5bc8d904e7519eaab9e0
MindaugasK <mindaugas.krup@gmail.com>
2022-11-27 20:38:47 +0200

    Update RoutePaths.js

--- ASP.NETCoreWebApplication/ClientApp/src/utils/RoutePaths.js
+++ ASP.NETCoreWebApplication/ClientApp/src/utils/RoutePaths.js
@@ -1,4 +1,4 @@
-﻿export const URI = 'https://localhost:5001'
+﻿export const URI = process.env.PUBLIC_URL;
 
 export const getHousingObjects = URI+'/housingitems/get';
 export const getHousingObjectsMySQL = URI+'/housingitems/sqlget';
